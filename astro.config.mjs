7d752117e736f1a272343dff5a8e07fb91e7072c
inayath <inayath.edu@gmail.com>
2024-06-20 13:26:57 +0530

    configure github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://assisilab.github.io',
-  base: '/assilab.github.io',
+  // base: '/assilab.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });

671c70ef2f4a32af5669ca8d7c662285584eed54
kalpaiiserpune <kalpa@sac.iiserpune.ac.in>
2025-01-11 19:16:37 +0530

    Put up two posts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Kalpa, IISER Pune';
-export const SITE_DESCRIPTION = 'Student Media Body';
+export const SITE_TITLE = 'Kalpa IISER Pune';
+export const SITE_DESCRIPTION = 'Official Student Media Body of IISER Pune';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
